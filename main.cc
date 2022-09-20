1919c52544a258a7dfdc14461d434cdc1969b566
InformalFormalities <MyCollegeBoardAndSchoolEmail@gmail.com>
2022-09-20 12:11:03 -0700

    Main program comment

---
+++ main.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+//Need to include colors.
+
+int main() {
+
+	//I'm thinking we should try to have main be a driver program, with
+	//functions that carry out the majority of the program.
+	//This will make it far easier to test our own codes independantly.
+	//Not everything will need a function (such as dialogue), but at least for
+	//the map, dungeon/puzzles, etc. it'll be a good idea.
+
+}
